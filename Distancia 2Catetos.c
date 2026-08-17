8f8917123d0513b89c48f6aea33db9a6488fa50c
Matheus Augusto Paixão <mataugp@gmail.com>
2026-08-17 09:30:35 -0300

    Add files via upload

---
+++ Distancia 2Catetos.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+/*	
+	int n;
+	printf("Entre com o valor de N: ");
+	scanf("%d", &n);
+	
+	bit64 = n%2; 
+	resultado = n/2; 
+	
+	bit32 = resultado%2; 
+	resultado = resultado/2;
+	
+	bit16 = resultado%2; 
+	resultado = resultado/2;
+	
+	bit8 = resultado%2; 
+	resultado = resultado/2;
+	
+	bit4 = resultado%2; 
+	resultado = resultado/2;
+	
+	bit2 = resultado%2; 
+	resultado = resultado/2;
+	printf("o numero %d em binario = %d%d%d%d%d%d%d", n, resultado%2,  bit2, bit32, bit4, bit8, bit16, bit32, bit64 );
+	*/
+	
+	int x1, x2, y1, y2
+	float dist; 
+	
+	printf("Entre com os valores para p1(x1,y1)")
+	scanf("%d" , &x1);
+	scanf("%d" , &y1);
+	
+	printf("Entre com os valores para p2(x2,y2)")
+	scanf("%d" , &x2);
+	scanf("%d" , &y2);
+	
+	printf("Leitura: (%d,%d)",x1, y1);
+	
+	return 0;
+	
+}
